31371e8060967a92cf5eeb8a8eab459d00ffddf2
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-16 13:33:35 +0530

    Nest related Home components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import HomeButton from "../HomeButton/HomeButton";
+import Button from "./Button/Button";
 
 const Home = () => {
   return (
     <main className="flex-1 flex items-center justify-center">
       <div className="flex flex-col items-center space-y-4">
-        <HomeButton placeholder="Play" />
-        <HomeButton placeholder="Favorites" />
+        <Button placeholder="Play" />
+        <Button placeholder="Favorites" />
       </div>
     </main>
   );
