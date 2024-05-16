e47da9ba7050fffb953f261d3183820373976c86
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-16 14:42:13 +0530

    Add game logic to fetch random pokemon

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Game = () => {
-  return <div>Welcome to the game</div>;
+  const [pokemonId, setPokemonId] = useState(null);
+  const [pokemon, setPokemon] = useState({
+    image: "",
+    name: "",
+    types: [],
+    abilities: [],
+  });
+
+  const randomIdGenerator = () => {
+    const total = 1025;
+    return Math.floor(Math.random() * total) + 1;
+  };
+
+  const fetchData = async () => {
+    const random = randomIdGenerator();
+    setPokemonId(random);
+
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+    );
+    const data = await response.json();
+
+    const abilities = data.abilities.map((ability) => ability.ability.name);
+
+    setPokemon((prevState) => ({
+      ...prevState,
+      name: data.name,
+      image: data.sprites.other.dream_world,
+      types: data.types.map((type) => type.type.name),
+      abilities: abilities,
+    }));
+
+    console.log("pokemon:", pokemon);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <main className="flex-1 flex items-center justify-center">
+      <div className="flex flex-col items-center space-y-4">
+        <h1>Welcome to the game</h1>
+      </div>
+    </main>
+  );
 };
 
 export default Game;
