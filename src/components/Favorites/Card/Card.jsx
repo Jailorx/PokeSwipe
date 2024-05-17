6120d5e9c5bae2a88c26a0c25996c765321c464c
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-17 13:25:30 +0530

    Add darkMode feature to Favorite component

--- src/components/Favorites/Card/Card.jsx
+++ src/components/Favorites/Card/Card.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useTheme } from "../../../context/ThemeContext";
 
 const Card = ({ name, image }) => {
+  const { theme, _ } = useTheme();
   return (
     <div className="my-4 mx-3">
-      <div className=" h-80 w-80 flex flex-col  justify-center items-center border-2 border-black rounded-3xl p-2   bg-gray-500">
+      <div
+        className={`h-80 w-80 flex flex-col justify-center items-center border-2 border-black rounded-3xl p-2 ${
+          theme ? "bg-gray-400" : "bg-gray-500"
+        }`}
+      >
         <div className="max-w-[150px] mx-auto">
           <img className="w-full" src={image} alt={`${name}_image`} />
         </div>
--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useData } from "../../context/LikeContext";
 import Card from "./Card/Card";
+import { useTheme } from "../../context/ThemeContext";
 
 const Favorites = () => {
+  const { theme, _ } = useTheme();
   const { likedPokemons } = useData();
 
   return (
-    <div className="flex-1 flex items-center justify-center">
+    <div className={`flex-1 flex items-center justify-center ${theme}`}>
       {likedPokemons.length > 0 ? (
         <div className="flex flex-wrap  justify-center items-center mt-4 min-mx-20">
           {likedPokemons.map(({ name, image }) => (
