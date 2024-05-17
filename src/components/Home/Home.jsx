675a20ea3067e75492c33b8f165b08e9983797cf
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-17 13:27:10 +0530

    Add DarkMode feature to Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../../context/ThemeContext";
 const Home = () => {
+  const { theme, _ } = useTheme();
   return (
-    <main className="flex-1 flex items-center justify-center">
+    <main className={`flex-1 flex items-center justify-center ${theme}`}>
       <div className="flex flex-col items-center space-y-4">
         <Link className="homeBtn " to="/game">
           Play
