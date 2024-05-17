03f8a66f69bd65c53b0f74777356b02f59d4fcc6
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-17 14:28:58 +0530

    Add feature listing favorite pokemons from game card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,26 +2,39 @@ import React from "react";
 import Choice_Button from "./Choice_Button/Choice_Button";
 import Attribute from "./Attribute/Attribute";
 import { useData } from "../../context/LikeContext";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaRegHeart } from "react-icons/fa";
+import { useTheme } from "../../context/ThemeContext";
 
 const Card = ({ info, nextPokemon }) => {
   const { name, abilities, types, image } = info;
 
-  const { likedPokemons, addLikedPokemon } = useData();
+  const { __, addLikedPokemon } = useData();
+  const { theme, _ } = useTheme();
 
   const handleLikedPokemon = (name, image) => {
     const pokemon = { name, image };
     addLikedPokemon(pokemon);
     nextPokemon();
-    // console.log("liked clicked");
   };
   const handleDislikedPokemon = () => {
-    // console.log("disliked clicked");
     nextPokemon();
   };
 
   return (
-    <div className="flex flex-col justify-center  gap-3">
-      <div className="max-w-[400px] mx-auto">
+    <div className="flex flex-col justify-center gap-3 ">
+      {theme ? (
+        <Link to="/favorites">
+          <FaHeart size={50} />
+        </Link>
+      ) : (
+        <Link to="/favorites">
+          <FaRegHeart size={50} />
+        </Link>
+      )}
+
+      <div className="max-w-[300px] mx-auto">
         <img className="w-full" src={image} alt={`${name}_image`} />
       </div>
 
