c95578eaa9fb4b2a3f82229ef5e61dfe8db543ef
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-05-17 13:34:55 +0530

    Fix lightMode color

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="bg-gray-400 min-h-screen flex flex-col">
+    <div className="bg-gray-300 min-h-screen flex flex-col">
       <Header />
       <Outlet />
     </div>
